bdaf884c53301402f31c7bf0d28aae5f25d20321
RohitKS7 <rohitkumarsuman2011india@gmail.com>
2022-07-16 13:15:40 +0530

    1 file modified, 1 added, 1 deleted

--- src/components/temp.jsx
+++ src/components/temp.jsx
@@ -1,10 +1,34 @@
 // https://api.openweathermap.org/data/2.5/weather?q=agra&appid=65c2a36f1d3f86adcd200d20c9003d69
 
 import React, { useState, useEffect } from 'react';
-// import Weathercard from './weathercard';
+import WeatherCard from './weatherCard';
 import './style.css';
 
 const Temp = () => {
+  const [searchValue, setSearchValue] = useState('agra');
+
+  const getWeatherInfo = async () => {
+    try {
+      // add API url
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=65c2a36f1d3f86adcd200d20c9003d69`;
+
+      // fetching the data
+      const response = await fetch(url);
+
+      // converting the data to json format
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // by default at first render, weather of Agra must be shown in app
+  useEffect(() => {
+    getWeatherInfo;
+  }, []);
+
   return (
     <>
       <div className="wrap">
@@ -15,11 +39,14 @@ const Temp = () => {
             autoFocus
             id="search"
             className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
 
           <button
             className="searchButton"
             type="button"
+            onClick={getWeatherInfo}
           >
             Search
           </button>
@@ -27,6 +54,7 @@ const Temp = () => {
       </div>
 
       {/* our Weather Card */}
+      <WeatherCard />
     </>
   );
 };
